67703d36100e59209911f5486d79f4c1ce11176c
João Pedro Oliveira <joaopedro.oliveirajava@gmail.com>
2024-03-27 23:26:51 -0300

    Little refatoring

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const defaultBranch = 'master';
 const standardsPath = 'src/Standards';
 const $standardsSelect = $("#standards-select");
 const $sniffsTypesSelect = $("#sniffs-types-select");
-let allSniffsTypesByStandard = {};
 
 let formatDataToSelect = (data, value, text) => {
     if (!data) {
@@ -20,86 +19,36 @@ let formatDataToSelect = (data, value, text) => {
     return dataFormatted;
 }
 
-const getAllStandards = async () => {
-    try {
-        const response = await fetch(`${defaultApiUrl}/${standardsPath}?ref=${defaultBranch}`);
-        const data = await response.json();
-
-        return data;
-    } catch (error) {
-        console.error('Error: ', error);
+$('#standards-select').select2({
+    ajax: {
+        url: `${defaultApiUrl}/${standardsPath}?ref=${defaultBranch}`,
+        dataType: 'json',
+        processResults: data => {
+            return {
+                results: formatDataToSelect(data, 'name', 'name')
+            }
+        }
     }
-}
-
-const getAllSniffsTypesByStandard = async () => {
-    try {
-        standards = await getAllStandards();
-
-        allSniffsTypesByStandard = await Promise.all(standards.map(async standard => {
-            const allSniffsTypesByStandardRequest = await fetch(`${defaultApiUrl}/${standardsPath}/${standard.name}/Sniffs?ref=${defaultBranch}`);
-            const allSniffsTypesByStandardResponse = await allSniffsTypesByStandardRequest.json();
-
-            return await Promise.all(allSniffsTypesByStandardResponse.map(async sniffType => {
-                return {
-                    standard: standard.name,
-                    sniffType: sniffType.name
-                }
-            }))
-        }));
-
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-}
-
-const fillStandardsSelect = async () => {
-    try {
-        const standards = await getAllStandards();
-
-        $standardsSelect.select2({
-            data: formatDataToSelect(standards, 'name', 'name')
-        });
-        $standardsSelect.trigger('change');
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-};
-
-getAllSniffsTypesByStandard();
-fillStandardsSelect();
-
-$standardsSelect.on('change', async () => {
-    if (!allSniffsTypesByStandard) {
-        return;
-    }
-    $sniffsTypesSelect.select2().empty();
-
-    let selectedStandard = $standardsSelect.val();
-
-    let sniffsTypesByStandard = allSniffsTypesByStandard.filter(standard => {
-        return standard[0].standard === selectedStandard;
-    });
-
-    $sniffsTypesSelect.select2({
-        data: formatDataToSelect(sniffsTypesByStandard[0], 'sniffType', 'sniffType')
-    });
 });
 
-$sniffsTypesSelect.on('change', async () => {
-    let selectedStandard = $standardsSelect.val();
-    let selectedSniffType = $sniffsTypesSelect.val();
 
-    let sniffsByTypeAndStandardRequest = await fetch(`${defaultApiUrl}/${standardsPath}/${selectedStandard}/Sniffs/${selectedSniffType}?ref=${defaultBranch}`);
-    let sniffsByTypeAndStandardResponse = await sniffsByTypeAndStandardRequest.json();
+let urlSniffsTypesByStandard;
 
-    $('#sniffs-table').DataTable({
-        data: await sniffsByTypeAndStandardResponse,
-        columns: [
-            { ata: 'name' }
-        ]
-    });
+$('#standards-select').on("change", function (e) {
+    urlSniffsTypesByStandard = `${defaultApiUrl}/${standardsPath}/${this.value}/Sniffs?ref=${defaultBranch}`;
 });
 
-// obeto->download_url = https://raw.githubusercontent.com/PHPCSStandards/PHP_CodeSniffer/master/src/Standards/PSR1/Sniffs/Files/SideEffectsSniff.php
-// objeto->name = SideEffectsSniff.php;
-// Name - button to open modal with description - checkbox
+$('#sniffs-group-select').select2({
+    ajax: {
+        url: () => urlSniffsTypesByStandard,
+        dataType: 'json',
+        processResults: response => {
+            return {
+                results: formatDataToSelect(response, 'name', 'name')
+            }
+        }
+    }
+});
+
+// Pegar as a combinação Standard + Sniff Group fazer a requisição para obter todos os arquivos da pasta
+// Pegar todos os arquivos da pasta e ler localmente com fetch /Standards/{Stantard}/Sniffs/{SniffGroup}/Doc.xml
