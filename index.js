8d44bd34c607ef8ef7ecd263604e9366d2c42ae4
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2024-03-29 22:52:11 -0300

    Starting building the datatable

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ $('#standards-select').select2({
 
 let urlSniffsTypesByStandard;
 
-$('#standards-select').on("change", function (e) {
+$('#standards-select').on('change', function (e) {
     urlSniffsTypesByStandard = `${defaultApiUrl}/${standardsPath}/${this.value}/Sniffs?ref=${defaultBranch}`;
 });
 
@@ -50,5 +50,52 @@ $('#sniffs-group-select').select2({
     }
 });
 
-// Pegar as a combinação Standard + Sniff Group fazer a requisição para obter todos os arquivos da pasta
-// Pegar todos os arquivos da pasta e ler localmente com fetch /Standards/{Stantard}/Sniffs/{SniffGroup}/Doc.xml
+const parser = new DOMParser();
+
+$('#sniffs-group-select').on('change', async function (e) {
+    let selectedStandard = $standardsSelect.val();
+    let selectedSniffGroup = this.value;
+
+    let sniffsByStandardAndGroupURL = `${defaultApiUrl}/${standardsPath}/${selectedStandard}/Docs/${selectedSniffGroup}?ref=${defaultBranch}`;
+    
+    try {
+        let sniffsByStandardAndGroupRequest = await fetch(sniffsByStandardAndGroupURL);
+        let sniffsByStandardAndGroup = await sniffsByStandardAndGroupRequest.json();
+
+        let formattedDataToDatatable = await Promise.all(sniffsByStandardAndGroup.map(async sniff => {
+            let sniffDocsPath = sniff.path.replace('src/', '');
+
+            let sniffDocsFile = await fetch(sniffDocsPath);
+            let sniffDocsFileContent = await sniffDocsFile.text();
+            let sniffDocsFileParsed = parser.parseFromString(sniffDocsFileContent, 'text/xml');
+
+            return {
+                title: sniffDocsFileParsed.querySelector('documentation').getAttribute('title'),
+                description: sniffDocsFileParsed.querySelector('standard').textContent
+                //example: sniffDocsFileParsed.querySelector('code_comparison')
+            }
+        }));
+
+        let SniffDataTable = new DataTable('#sniffs-table', {
+            data: formattedDataToDatatable,
+            columnDefs: [
+                {
+                    "defaultContent": "-",
+                    "targets": "_all"
+                },
+                {
+                    data: 'title',
+                    targets: 0
+                },
+                {
+                    data: 'description',
+                    targets: 1
+                }
+            ]
+        })
+
+    } catch (error) {
+        console.log('Error', error)
+    }
+
+});
